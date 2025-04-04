1d87f589fb9d5622de15a28a047764b871a3fa6a
Edom <edomkmengich@gmail.com>
2025-04-04 10:40:37 -0700

    First Push

---
+++ First Greeting code.py
@@ -0,0 +1,12 @@
+greeting = "Hello Mf!"
+print(greeting)
+
+cont1 = '||||||||||||||||||||||'
+cont2 = '|||| KARIBU CHIEF ||||'
+cont3 = '|||||______________|||'
+print()
+print(cont1)
+print(cont3)
+print(cont2)
+print(cont3)
+print(cont1)
